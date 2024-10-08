ca825c36cd8038bf2f49dd0f802d7500869beef4
Prasad Lokhande <112815540+Prasadlokhande-880@users.noreply.github.com>
2024-10-08 10:26:14 +0530

    Update MenuItems.js

--- src/component/MenuItems.js
+++ src/component/MenuItems.js
@@ -1,24 +1,24 @@
 const menuItems = [
-    {
-      title: "Home",
-      url: "#home",
-      cName: "nav-links"
-    },
-    {
-      title: "Services",
-      url: "#Services",
-      cName: "nav-links"
-    },
-    {
-      title: "Products",
-      url: "#Products",
-      cName: "nav-links"
-    },
-    {
-      title: "Contact",
-      url: "#Contact",
-      cName: "nav-links"
-    }
-  ];
+  {
+    title: "Home",
+    url: "#home",
+    className: "nav-links"
+  },
+  {
+    title: "Services",
+    url: "#services",
+    className: "nav-links"
+  },
+  {
+    title: "Products",
+    url: "#products",
+    className: "nav-links"
+  },
+  {
+    title: "Contact",
+    url: "#contact",
+    className: "nav-links"
+  }
+];
 
-  export default menuItems;
+export default menuItems;
